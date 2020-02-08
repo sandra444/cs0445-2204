b707d6e2ab8fdd80ff22a2c0c5253072b26fddf3
skhattab <skhattab@cs.pitt.edu>
2020-02-08 16:40:08 -0500

    Lecture 9 - 2/5/2020

--- Stack/BalanceChecker.java
+++ Stack/BalanceChecker.java
@@ -10,7 +10,46 @@ public class BalanceChecker{
     Scanner scan = new Scanner(System.in);
     System.out.println("Enter a string to check its balanace:");
     String input = scan.nextLine();
-    System.out.println(checkBalance(input));
+    if(!checkBalance(input)){
+      System.out.println("Unbalanced brackets!");
+    } else{
+      System.out.println(evaluateInfix(input));
+    }
+  }
+
+  private Double evaluateInfix(String infix){
+    StackInterface<Character> operatorStack =
+          new LinkedStack<Character>();
+    StackInterface<Double> operandStack =
+      new LinkedStack<Double>();
+
+    for(int i=0; i<infix.length(); i++){
+      char c = infix.charAt(i);
+      if(isOperand(c)){
+        operandStack.push(Double.valueOf(c));
+      } else if(isOperator(c)){
+        if(c == ')'){
+          while(!operatorStack.isEmpty()){
+            char operator = operatorStack.pop();
+            if(operator == '('){
+              break;
+            }
+            Double secondOperand = operandStack.pop();
+            Double firstOperand = operandStack.pop();
+            Double result = compute(operator, firstOperand, secondOperand);
+            operandStack.push(result);
+          }
+        } else if(c == '(' || c == '^' || operatorStack.isEmpty()){
+          operatorStack.push(c);
+        } else {
+          //TO-O Pop until top of the operator stack is of lower precedence
+
+        }
+
+      }
+    }
+    //TODO Pop until operator stack is empty
+    return null;
   }
 
   private boolean checkBalance(String input){
@@ -55,4 +94,35 @@ public class BalanceChecker{
            (open == '(' && close == ')');
   }
 
+  private boolean isOperand(char c){
+    return c == '0' || c == '1' || c == '2' ||
+           c == '3' || c == '4' || c == '5' ||
+           c == '6' || c == '7' || c == '8' || c == '9';
+  }
+
+  private boolean isOperator(char c){
+    return c == '(' || c == ')' || c == '+' ||
+           c == '-' || c == '*' || c == '/' || c == '^';
+  }
+
+  private Double compute(char operator,
+                         Double firstOperand,
+                         Double secondOperand){
+    Double result = null;
+    switch(operator){
+      case '+':
+        result = firstOperand + secondOperand;
+      case '-':
+        result = firstOperand - secondOperand;
+      case '*':
+        result = firstOperand * secondOperand;
+      case '/':
+        result = firstOperand / secondOperand;
+      case '^':
+        result = Math.pow(firstOperand, secondOperand);
+      default:
+        System.out.println("Unknown operator");
+    }
+    return result;
+  }
 }
